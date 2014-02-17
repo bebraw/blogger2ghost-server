5c0b43b249a6df17d041d16f0c401fa569c9bc08
Juho Vepsalainen <bebraw@gmail.com>
2014-02-17 21:05:02 +0100

    In case url has a subdomain, don't inject www

--- tests/test_get_blog_url.js
+++ tests/test_get_blog_url.js
@@ -5,14 +5,21 @@ var suite = require('suite.js');
 var getBlogUrl = require('../lib/get_blog_url');
 
 
+var suffix = '/feeds/posts/default?alt=json&max-results=10000';
+
 suite(getBlogUrl, suite.multiple([
     'foo.info',
     'http://foo.info',
     'www.foo.info',
     'http://www.foo.info'
-], 'http://www.foo.info/feeds/posts/default?alt=json&max-results=10000'));
+], 'http://www.foo.info' + suffix));
 
 suite(getBlogUrl, suite.multiple([
     'https://foo.info',
     'https://www.foo.info'
-], 'https://www.foo.info/feeds/posts/default?alt=json&max-results=10000'));
+], 'https://www.foo.info' + suffix));
+
+suite(getBlogUrl, suite.multiple([
+    'http://blog.jsdelivr.com',
+    'blog.jsdelivr.com'
+], 'http://blog.jsdelivr.com' + suffix));
